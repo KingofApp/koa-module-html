bc2b3c27febcb5c7f4852819c6d14df483127db2
Darryl <darrylgarcia96gmail.com>
2018-07-10 18:00:01 +0200

    Bugs fixed

--- clearCssDirective.js
+++ clearCssDirective.js
@@ -5,19 +5,17 @@ angular.directive('koastyles', function () {
         link: function (scope, element, attrs) {
             scope.$watch('code', function (newVal) {
                 if (newVal) {
-                    //console.log("css before transformation", newVal);
-                    newVal = newVal.replace(/\/\*((?!\*\/).)*\*\//g, "");
-                    newVal = newVal.replace(/\s{2,}/g, " ");
-                    newVal = newVal.replace(/\s*([:;{}])\s*/g, "$1");
-                    newVal = newVal.replace(/;}/g, "}");
+                    //minify the code
+                    newVal = newVal.replace(/\/\*((?!\*\/).)*\*\//g, ""); //delete comments
+                    newVal = newVal.replace(/\s{2,}/g, " ");//delete all whitespace
+                    newVal = newVal.replace(/\s*([:;{}])\s*/g, "$1"); //delete whitespace before and after of the special characters
+                    newVal = newVal.replace(/;}/g, "}");//erase semicolon of the last property of the block
                     
-                    newVal = "." + attrs.setclass + " " + newVal;
-                    newVal = newVal.replace(/[}](?!$)/g, "} ." + attrs.setclass + " ");
-                    
-                    //debug line
-                    //console.log("css after transformation", newVal);
-                    
-                    scope.styleCode = newVal
+                    //encapsulate the code
+                    newVal = "." + attrs.setclass + " " + newVal;//put encapsulation class in front of the first css selector
+                    newVal = newVal.replace(/[}](?!$)/g, "} ." + attrs.setclass + " ");//put encapsulation class in front of the rest css selectors
+
+                    scope.styleCode = newVal; //assigning the minded CSS to the "styleCode" variable
                 }
             });
         }
